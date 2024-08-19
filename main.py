a3b47af1c11784c31b9763fb77b7e2fa3da55a7d
estvv <esteve.bascunana@epitech.eu>
2024-08-19 15:00:44 +0200

    [UPDATE] can resolve basics

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from src.sudoku import *
 
 if __name__ == "__main__":
     grid = Sudoku()
-    grid.createSudokuGrid("./grids/easy.sudoku")
+    grid.createSudokuGrid("./grids/puzzles/easy.sudoku")
     grid.solver()
-    grid.displaySudokuGrid(grid.sudoku_grid)
+    grid.writeGrid()
--- src/sudoku.py
+++ src/sudoku.py
@@ -3,7 +3,8 @@ from src.logger import *
 class Sudoku():
     logger: logging
     sudoku_grid: list[any] = []
-    trying_grid: list[dict] = [{}]
+    trying_grid: list[dict] = []
+    filename: str
 
     def __init__(self):
         self.setLogger()
@@ -25,12 +26,12 @@ class Sudoku():
                 file = file.replace('\n', ' ')
         except:
             file = ""
+        self.filename = filename[1:]
         self.sudoku_grid = file.split(' ')
-        self.sudoku_grid.insert(0, "3x3x3")
 
     def gridIsCorrect(self) -> bool:
         res: bool = True
-        for i in range(1, len(self.sudoku_grid)):
+        for i in range(len(self.sudoku_grid)):
             try:
                 if int(self.sudoku_grid[i]) > 9:
                     self.logger.error("BAD GRID")
@@ -45,7 +46,7 @@ class Sudoku():
         return res
 
     def clearGrid(self) -> None:
-        i: int = 1
+        i: int = 0
         while i < len(self.sudoku_grid):
             try:
                 int(self.sudoku_grid[i])
@@ -54,9 +55,9 @@ class Sudoku():
                 self.sudoku_grid.pop(i)
 
     def initSudodukoGrid(self) -> None:
-        for i in range(1, len(self.sudoku_grid)):
+        for i in range(len(self.sudoku_grid)):
             self.sudoku_grid[i] = int(self.sudoku_grid[i])
-            self.trying_grid.append({0: 'Y', 1: 'Y', 2: 'Y', 3: 'Y', 4: 'Y', 5: 'Y', 6: 'Y', 7: 'Y', 8: 'Y', 9: 'Y'})
+            self.trying_grid.append({1: 'Y', 2: 'Y', 3: 'Y', 4: 'Y', 5: 'Y', 6: 'Y', 7: 'Y', 8: 'Y', 9: 'Y'})
 
     def createSudokuGrid(self, filename) -> None:
         self.openGrid(filename)
@@ -67,33 +68,76 @@ class Sudoku():
         self.displaySudokuGrid(self.sudoku_grid)
 
     def displaySudokuGrid(self, grid: list) -> None:
-        print(self.sudoku_grid[0])
-        for i in range(1, len(grid)):
-            print(f"{grid[i]}", end = "")
-            if i % 3 == 0 and i % 9 != 0 and i != 0:
-                print(" | ", end = "")
-            elif i % 27 == 0 and i != len(grid) - 1 and i != 0:
-                print("\n------+-------+------")
-            elif i % 9 == 0 and i != 0:
+        for i in range(len(grid)):
+            if i % 9 == 0 and i != 0:
                 print("")
+            if i % 27 == 0 and i != 0:
+                print("------+-------+------")
+            if i % 3 == 0 and i % 9 != 0:
+                print(" | ", end="")
+            if (i + 1) % 9 == 0 or (i + 1) % 3 == 0:
+                print(grid[i], end="")
             else:
-                print(" ", end = "")
+                print(grid[i], end=" ")
+        print("")
 
     def solver(self):
-        for i in range(1, len(self.sudoku_grid)):
-            if self.sudoku_grid == 0: pass
-            self.putAnnotations(0, i - 1)
+        for i in range(5):
+            for i in range(len(self.sudoku_grid)):
+                if self.sudoku_grid[i] == 0:
+                    pass
+                self.putAnnotations(self.sudoku_grid[i], i)
+
+            for i in range(len(self.sudoku_grid)):
+                if self.sudoku_grid != 0:
+                    pass
+                self.checkAnnotations(i)
 
     def putAnnotations(self, num, idx):
-        row_lim: tuple[int, int] = [idx - (idx % 9) + 1, idx + (9 - (idx % 9)) + 1]
-        col_lim: tuple[int, int] = [idx % 9 + 1, idx % 9 + 74]
+        row_lim: tuple[int, int] = (idx - (idx % 9), idx - (idx % 9) + 9)
+        col_lim: tuple[int, int] = (idx % 9, (idx % 9) + 73)
+        box_start = (idx // 27) * 27 + ((idx % 9) // 3) * 3
+        box_lim: tuple[int, int] = (box_start, box_start + 20)
 
         for i in range(row_lim[0], row_lim[1], 1):
             if self.sudoku_grid[i] == 0:
                 self.trying_grid[i][num] = 'N'
-                self.sudoku_grid[i] = 9
 
         for i in range(col_lim[0], col_lim[1], 9):
             if self.sudoku_grid[i] == 0:
                 self.trying_grid[i][num] = 'N'
-                self.sudoku_grid[i] = 6
+
+        for i in range(box_lim[0], box_lim[1], 9):
+            for j in range(3):
+                if self.sudoku_grid[i + j] == 0:
+                    self.trying_grid[i + j][num] = 'N'
+
+    def checkAnnotations(self, idx):
+        is_good: list[int] = []
+        for num, state in self.trying_grid[idx].items():
+            if state == 'Y':
+                is_good.append(num)
+
+        if len(is_good) == 1:
+            self.sudoku_grid[idx] = is_good[0]
+            self.putAnnotations(self.sudoku_grid[idx], idx)
+
+    def writeGrid(self):
+        index = self.filename.index(".")
+        filename = "." + self.filename[:index] + "_solution" + self.filename[index:]
+        filename = filename.replace("puzzles", "solutions")
+        try:
+            with open(filename, "w") as f:
+                for i in range(len(self.sudoku_grid)):
+                    if i % 9 == 0 and i != 0:
+                        f.write("\n")
+                    if i % 27 == 0 and i != 0:
+                        f.write("------+-------+------\n")
+                    if i % 3 == 0 and i % 9 != 0:
+                        f.write(" | ")
+                    if (i + 1) % 9 == 0 or (i + 1) % 3 == 0:
+                        f.write(f"{self.sudoku_grid[i]}")
+                    else:
+                        f.write(f"{self.sudoku_grid[i]} ")
+        except:
+            self.logger.error(f"CAN'T CREATE {filename}")
